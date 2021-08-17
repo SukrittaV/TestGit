83491fef205786a3aa7f98ef03220139f7f1fda3
SukrittaV <sukritta.v5@gmail.com>
2021-08-17 16:06:15 +0700

    เพิ่ม function + แก้ bug [fix value]

--- test github.py
+++ test github.py
@@ -1,25 +1,37 @@
 # ราคา BTC ล่าสุด
-from forex_python.bitcoin import BtcConverter
-b = BtcConverter()
-bitCoinTHB = b.get_latest_price('THB')
-print('BTC price :',bitCoinTHB,'THB')
+def ShowBTCPrice():
+    from forex_python.bitcoin import BtcConverter
+    b = BtcConverter()
+    bitCoinTHB = b.get_latest_price('THB')
+    print('BTC price :',bitCoinTHB,'THB')
 
 # ถ้า BTC > THB +2% = ขาย BTC
 # ถ้า BTC < THB -2% = ซื้อ BTC
-percentage = 2
+def rebalance():
+    from forex_python.bitcoin import BtcConverter
+    b = BtcConverter()
+    bitCoinTHB = b.get_latest_price('THB')
+    percentage = 2  # % ที่จะ re-balance
+    thb_Value = float(input('THB balance :'))  # มูลค่าเงินบาทที่มี
+    btc_balance = float(input('BTC balance[Coin] : '))  # จำนวนเหรียญ BTC ที่มี
+    btc_Value = bitCoinTHB * btc_balance  # มูลค่า BTC ที่มี
+    print('BTC balance [value]', btc_Value)
+    fix_Value = (btc_Value + thb_Value)/2 # มูลค่าส่วนต่างของ 2 asset
+    print('fix value :',fix_Value)
+    print('fix value percentage :',fix_Value * (1+(percentage/100)))
+    # ขายออก
+    if btc_Value > (fix_Value * (1+(percentage/100))):  # * (1+(percentage/100)))
+        amount = btc_Value - fix_Value
+        print(f'Sell BTC {amount} THB')
+    # ซื้อเข้า
+    elif btc_Value < (fix_Value * (1-(percentage/100))):
+        amount = fix_Value - btc_Value
+        print(f'Buy BTC {amount} THB')
+    else:
+        print('wait . . .')
 
-THB_Value = 100000000  # มูลค่าเงินบาทที่มี
-btc_balance = 0.0001  # จำนวนเหรียญ BTC ที่มี
-btc_Value = bitCoinTHB * btc_balance  # มูลค่า BTC ที่มี
-fix_Value = 50  # % ที่จะ re-balance
-if btc_Value > (fix_Value * (1+(percentage/100))):  # * (1+(percentage/100)))
-    amount = btc_Value - fix_Value
-    print(f'Sell BTC {amount} THB')
-elif btc_Value < (fix_Value * (1+(percentage/100))):
-    amount = fix_Value - btc_Value
-    print(f'Buy BTC {amount} THB')
-else:
-    print('wait . . .')
+ShowBTCPrice()
+rebalance()
 
 
 
