0c3a3739deb4af8be2b744d1cdf096aaa7b298a4
SukrittaV <sukritta.v5@gmail.com>
2021-08-17 16:19:20 +0700

    แก้ไข fix value เอาตัวcheck ออก เพิ่มคำ

--- test github.py
+++ test github.py
@@ -18,17 +18,19 @@ def rebalance():
     print('BTC balance [value]', btc_Value)
     fix_Value = (btc_Value + thb_Value)/2 # มูลค่าส่วนต่างของ 2 asset
     print('fix value :',fix_Value)
-    print('fix value percentage :',fix_Value * (1+(percentage/100)))
+    #print('fix value percentage :',fix_Value * (1+(percentage/100)))
     # ขายออก
     if btc_Value > (fix_Value * (1+(percentage/100))):  # * (1+(percentage/100)))
         amount = btc_Value - fix_Value
+        #print(f'{btc_Value} > {(fix_Value * (1+(percentage/100)))}') check ค่าส่วนต่างถูกต้อง = ต้องมากกว่า 2%
         print(f'Sell BTC {amount} THB')
     # ซื้อเข้า
     elif btc_Value < (fix_Value * (1-(percentage/100))):
         amount = fix_Value - btc_Value
+        #print(f'{btc_Value} < {(fix_Value * (1 + (percentage / 100)))}') check ค่าส่วนต่างถูกต้อง = ต้องน้อยกว่า 2%
         print(f'Buy BTC {amount} THB')
     else:
-        print('wait . . .')
+        print(f'wait . . . ยังไม่ถึง{percentage} percentage')
 
 ShowBTCPrice()
 rebalance()
